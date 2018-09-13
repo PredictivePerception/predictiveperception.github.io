43c41762fbc5c9dab8e1f1455bc7416e565769a7
madigan-99 <fymadigan@outlook.com>
2018-09-12 20:16:00 -0700

    send

--- script.js
+++ script.js
@@ -12,14 +12,3 @@ function tempAlert(){
 	window.setTimeout("fade();", 50);
 }
 
-
-function tempAlertFast()
-{
- var pop = document.getElementsByClassName('popUp');
-  var newStyles = document.createElement('style')
-       document.head.append(newStyles);
-       newStyles.innerHTML = ".popUp{visibility:visible}";
-	window.setTimeout("fade();", 50);
-}
-
-
